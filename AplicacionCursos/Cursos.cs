d9967c25a336b99927443d3577de39c17f6cbcdc
Jesus <jesuseduardocast18@gmail.com>
2025-01-20 06:38:30 -0400

    Add: Class cursos

---
+++ AplicacionCursos/Cursos.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ddd
+ * Date: 1/20/2025
+ * Time: 6:37 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AplicacionCursos
+{
+	/// <summary>
+	/// Description of Cursos.
+	/// </summary>
+	public class Cursos
+	{
+		public Cursos()
+		{
+		}
+	}
+}
