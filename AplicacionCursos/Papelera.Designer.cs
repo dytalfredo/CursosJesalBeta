ece354951baa81c2681fac6a9a19ef41ec7e7e38
Jesus <jesuseduardocast18@gmail.com>
2025-01-21 02:47:42 -0400

    Remove groupbox exmaple

--- AplicacionCursos/Papelera.Designer.cs
+++ AplicacionCursos/Papelera.Designer.cs
@@ -39,48 +39,7 @@ namespace AplicacionCursos
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Papelera));
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.checkBox1 = new System.Windows.Forms.CheckBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.BackColor = System.Drawing.Color.Linen;
-			this.groupBox1.Controls.Add(this.checkBox1);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-			this.groupBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox1.Location = new System.Drawing.Point(0, 0);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(600, 51);
-			this.groupBox1.TabIndex = 0;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Hola";
-			// 
-			// checkBox1
-			// 
-			this.checkBox1.Dock = System.Windows.Forms.DockStyle.Right;
-			this.checkBox1.Location = new System.Drawing.Point(493, 22);
-			this.checkBox1.Name = "checkBox1";
-			this.checkBox1.Size = new System.Drawing.Size(104, 26);
-			this.checkBox1.TabIndex = 1;
-			this.checkBox1.Text = "Activar";
-			this.checkBox1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-			this.checkBox1.UseVisualStyleBackColor = true;
-			this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
-			// 
-			// label1
-			// 
-			this.label1.Dock = System.Windows.Forms.DockStyle.Left;
-			this.label1.Location = new System.Drawing.Point(3, 22);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(400, 26);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// Papelera
 			// 
@@ -91,17 +50,12 @@ namespace AplicacionCursos
 			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(600, 375);
-			this.Controls.Add(this.groupBox1);
 			this.DoubleBuffered = true;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Papelera";
 			this.Text = "Papelera";
 			this.Load += new System.EventHandler(this.PapeleraLoad);
-			this.groupBox1.ResumeLayout(false);
 			this.ResumeLayout(false);
 		}
-		private System.Windows.Forms.CheckBox checkBox1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.GroupBox groupBox1;
 	}
 }
